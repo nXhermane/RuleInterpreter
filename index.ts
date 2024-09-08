3e9298c0e46dca59e22d949e28a01261319c981a
nXhermane <nxhermane@gmail.com>
2024-09-08 16:22:49 +0100

    jlh

--- index.ts
+++ index.ts
@@ -1,4 +1,3 @@
-import { ExpressionConstructor } from "./expression/ExpressionConstructor";
 import { FormularParser } from "./parser/FormularParser";
 import { Tokeniser } from "./tokeniser/FormularTokeniser";
 import { FormularInterpreter } from "./interpreter/FormularInterpreter";
@@ -17,42 +16,10 @@ type Data = {
    taille: number;
    niveauActivite: number;
 };
-
-//
-
-const exp = ExpressionConstructor.addition<Data>(
-   ExpressionConstructor.substration<Data>(
-      ExpressionConstructor.addition<Data>(
-         ExpressionConstructor.multiplication<Data>(
-            ExpressionConstructor.literalValue<Data>(10),
-            ExpressionConstructor.fieldReference<Data, number>("poids"),
-         ),
-         ExpressionConstructor.multiplication<Data>(
-            ExpressionConstructor.literalValue<Data>(6.25),
-            ExpressionConstructor.fieldReference<Data, number>("taille"),
-         ),
-      ),
-      ExpressionConstructor.multiplication<Data>(
-         ExpressionConstructor.literalValue<Data>(5),
-         ExpressionConstructor.fieldReference<Data, number>("age"),
-      ),
-   ),
-   ExpressionConstructor.multiplication<Data>(
-      ExpressionConstructor.condition<Data, number>(
-         ExpressionConstructor.equality<Data, string>(
-            ExpressionConstructor.fieldReference<Data, string>("sexe"),
-            ExpressionConstructor.literalValue<Data, string>("H"),
-         ),
-         ExpressionConstructor.literalValue(5),
-         ExpressionConstructor.literalValue(-161),
-      ),
-      ExpressionConstructor.fieldReference<Data, number>("niveauActivite"),
-   ),
-);
 const formula = `(10 * poids) + (6.25 * taille) - (5 * age ) + ((sexe == "H" ? 5 : (-161)) * niveauActivite)`;
 
 const fTokeniser = new Tokeniser();
 const fParser = new FormularParser();
 const fInterpreter = new FormularInterpreter();
 const astTree = fParser.execute(fTokeniser.execute(formula))
-fInterpreter.execute<Data>(astTree, data);
+console.log(fInterpreter.execute<Data>(astTree, data))
